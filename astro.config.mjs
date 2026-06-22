aa9661f57f67f33f85c3e6aae73efa1ea1af10eb
nicholasconnors <nicholas.connors@mail.mcgill.ca>
2026-06-22 15:58:19 -0400

    Update site path in config

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://nicholasconnors.github.io',
+    site: 'https://nicholasconnors.space',
     base: '/nicholasconnors.space'
 });
